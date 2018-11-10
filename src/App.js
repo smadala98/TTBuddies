9582aecda6cc94ba9a89737d6ad045832ffa2f23
Andrew Arifin <andrewarifin@Andrews-MacBook-Pro-2.local>
2018-11-10 15:06:18 -0800

    logo

--- src/App.js
+++ src/App.js
@@ -1,28 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './img/cropped_rose.png';
 import './App.css';
 
-class App extends Component {
+class Landing extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="Landing">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <img className="rose" src={logo} alt='rose'/>
+          <h1>Welcome to TT Buddies</h1>
         </header>
       </div>
     );
   }
 }
 
-export default App;
+export default Landing;
